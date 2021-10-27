f1431d9e8ff900bfd05beadc1332c2b72cbbc7fd
EvgeniyRuban <eugene_1997@inbox.ru>
2021-10-28 05:50:13 +0600

    Complite lesson4, task1
    Add Building.cs

---
+++ OOP_GB/OOP_GB/Building.cs
@@ -0,0 +1,87 @@
+﻿
+
+namespace OOP_GB
+{
+    public sealed class Building
+    {
+        private uint _number;
+
+        private double _height;
+
+        private uint _floorsCount;
+
+        private uint _flatsCount;
+
+        private uint _entrancesCount;
+
+
+        private static uint _lastNumber;
+
+
+        public Building() : this(0, 0, 0, 0) { }
+
+        public Building(
+            double height,
+            uint floorsCount,
+            uint flatsCount,
+            uint entrancesCount)
+        {
+            _height = height;
+            _floorsCount = floorsCount;
+            _flatsCount = flatsCount;
+            _entrancesCount = entrancesCount;
+            _number = GenerateNumber();   
+        }
+
+
+        public uint Number => _number;
+
+        public double Height => _height;
+
+        public uint FloorsCount => _floorsCount;
+
+        public uint FlatsCount => _flatsCount;
+
+        public uint EntrancesCount => _entrancesCount;
+
+
+        public double GetFloorHeight(int floorNumber)
+        {
+            if (_floorsCount > 0)
+            {
+                return _height / _floorsCount * floorNumber;
+            }
+            return 0;
+        }
+
+        public uint GetFlatsCountPerEntrance()
+        {
+            if(_entrancesCount > 0)
+            {
+                return _flatsCount / _entrancesCount;
+            }
+            return 0;
+        }
+
+        public uint GetFlatsCountPerFloor()
+        {
+            if(_floorsCount > 0)
+            {
+                return _flatsCount / _floorsCount;
+            }
+            return 0;
+        }
+
+        public string GetInfo()
+        {
+            return $"Number: {_number}\n" +
+                $"Height: {_height}\n" +
+                $"FloorsCount: {_floorsCount}\n" +
+                $"FlatsCounr: {_flatsCount}\n" +
+                $"EntracesCount: {_entrancesCount}";
+        }
+
+
+        private static uint GenerateNumber() => ++_lastNumber;
+    }
+}
